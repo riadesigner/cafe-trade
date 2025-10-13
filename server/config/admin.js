5d395db5118b78db54b429f2e6bdddc0fcee408a
evgenii <evgenii@MacBook-Pro-evgenii.local>
2025-10-13 22:52:51 +1000

    переместил пре-коммит

---
+++ server/config/admin.js
@@ -0,0 +1,11 @@
+const UsersService = require('../users/users.service');
+module.exports = async () => {
+  const addAdmins = async () => {
+    const arr = ['e.pogrebnyak@mail.ru'];
+    for (const email of arr) {
+      await UsersService.create({ email: email, role: 'administrator' });
+    }
+  };
+  const admins = await UsersService.findAdmins();
+  (!admins || admins.length === 0) && (await addAdmins());
+};
