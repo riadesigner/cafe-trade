4eecea27c4696a906a0a1711b960a5681f15e667
evgenii <evgenii@MacBook-Pro-evgenii.local>
2025-10-14 10:07:13 +1000

    ok

--- server/app.js
+++ server/app.js
@@ -5,13 +5,17 @@ const configureSessions = require('./config/sessions');
 const configurePassport = require('./config/passport');
 const configureCors = require('./config/cors');
 const configureAdmins = require('./config/admin');
-const requestLogger = require('./middleware/requestLogger')
+const requestLogger = require('./middleware/requestLogger');
 const configureDataBase = require('./config/db');
 const { sendError } = require('./middleware/utils');
-const { authErrorHandler, apiErrorHandler, fallbackErrorHandler } = require('./middleware/errorHandlers');
+const {
+  authErrorHandler,
+  apiErrorHandler,
+  fallbackErrorHandler,
+} = require('./middleware/errorHandlers');
 
-const path = require('path')
-const PUBLIC_PATH = path.join(__dirname+'/public');
+const path = require('path');
+const PUBLIC_PATH = path.join(__dirname + '/public');
 
 // -------------------
 //      APP INIT
@@ -24,21 +28,21 @@ configurePassport(app);
 configureCors(app);
 configureAdmins();
 
-app.use('/public',express.static(PUBLIC_PATH))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use('/public', express.static(PUBLIC_PATH));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
 
 // ------------
 //    ROUTS
 // ------------
-app.use('/auth',require('./auth/auth.routes')); // auth через яндекс и mailru  
-app.use('/api',require('./auth/auth-api.routes'));  // jwt
-app.use('/api',require('./users/users-api.routes')); // пользователи
+app.use('/auth', require('./auth/auth.routes')); // auth через яндекс и mailru
+app.use('/api', require('./auth/auth-api.routes')); // jwt
+app.use('/api', require('./users/users-api.routes')); // пользователи
 
-app.get('/', (req, res)=>{
-    const user = req.user;
-    res.render('index',{user});
+app.get('/', (req, res) => {
+  const user = req.user;
+  res.render('index', { user });
 });
 
 // 404 — если ни один маршрут не сработал
@@ -52,7 +56,6 @@ app.use('/api', apiErrorHandler(sendError));
 
 // Фолбэк
 app.use(fallbackErrorHandler);
-
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Сервер запущен на http://localhost:${process.env.PORT}`);
