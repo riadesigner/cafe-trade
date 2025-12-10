d395b4a1a29569716af8b9e562058b4c55e058f1
evgenii <evgenii@MacBook-Pro-evgenii.local>
2025-12-10 10:49:38 +1000

    добавил телефон и ссылку на главную в подвал

--- client/src/components/SiteFooter.jsx
+++ client/src/components/SiteFooter.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import { getPayloads } from '../utils/payloads';
+import logo from '../i/logo-1.png';
 
 export default function SiteFooter() {
   const { isAuthenticated, logout } = useAuth();
@@ -25,9 +26,30 @@ export default function SiteFooter() {
       <div className="section container is-max-desktop mt-6">
         <div className="level mt-6">
           <div className="level-item is-left">
-            <Link to="/">
-              <div className="ws-cafe-trade-logo">WS-CAFE-TRADE</div>
-            </Link>
+            <div className="level">
+              <Link to="/" className="level-item mb-3-mobile">
+                <div className="ws-cafe-trade-logo">WS-CAFE-TRADE</div>
+              </Link>
+              <a
+                className="level-item mb-3-mobile"
+                href="https://wallstreetvl.ru/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={logo}
+                  style={{
+                    width: '34px',
+                    height: 'auto',
+                    verticalAlign: 'middle',
+                  }}
+                />{' '}
+                WallStreet{' '}
+              </a>
+              <a className="level-item" href="tel:+79084449818">
+                +7 (908) 444‒98‒18
+              </a>
+            </div>
           </div>
           <hr className="mobile-only" />
           <div className="level-item is-right yo-footer-menu">
